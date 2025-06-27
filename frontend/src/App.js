ddce45442b130648e4fc9311a3af9ef31b3b8589
Qylahrzk <nuraqilahnur@gmail.com>
2025-06-28 03:22:18 +0800

    Modified Frontend App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,31 @@
+// src/App.js
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Login from './pages/Auth';
 import Bugs from './pages/Bugs';
 import Projects from './pages/Projects';
 import BugForm from './components/BugForm';
-import Bugs from './pages/Bugs';
+import Dashboard from './pages/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Login />} />
+        {/* Public route */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Protected routes */}
+        <Route path="/" element={<PrivateRoute><Layout /></PrivateRoute>}>
+          <Route index element={<Dashboard />} />
+          <Route path="dashboard" element={<Dashboard />} />
           <Route path="bugs" element={<Bugs />} />
+          <Route path="bugs/new" element={<BugForm />} />
+          <Route path="bugs/:id" element={<BugForm />} />
           <Route path="projects" element={<Projects />} />
-          <Route path="/bugs/new" element={<BugForm />} />
-          <Route path="/bugs/:id" element={<BugForm />} />
+
+          {/* Optional: match the "Manage" link in Dashboard's team section */}
+          <Route path="team" element={<div><h1>Team Management Page</h1></div>} />
         </Route>
       </Routes>
     </BrowserRouter>
