e1d221413cc661ef531479adf15a3083d1492be2
Qylahrzk <nuraqilahnur@gmail.com>
2025-06-29 15:45:59 +0800

    Update Frontend 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 // src/App.js
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import BugForm from './components/BugForm';
 import Login from './pages/Auth';
 import Bugs from './pages/Bugs';
 import Projects from './pages/Projects';
-import BugForm from './components/BugForm';
 import Dashboard from './pages/Dashboard';
+import CalendarPage from './pages/Calendar';
+import Discuss from './pages/Discuss';
 import PrivateRoute from './components/PrivateRoute';
 
 function App() {
@@ -23,6 +25,8 @@ function App() {
           <Route path="bugs/new" element={<BugForm />} />
           <Route path="bugs/:id" element={<BugForm />} />
           <Route path="projects" element={<Projects />} />
+          <Route path="calendar" element={<CalendarPage />} />
+          <Route path="discuss" element={<Discuss />} />
 
           {/* Optional: match the "Manage" link in Dashboard's team section */}
           <Route path="team" element={<div><h1>Team Management Page</h1></div>} />
